dbc9389b4ebb66e29a43856d34b201981b19b499
koprty <your_email@example.com>
2015-01-03 16:18:14 -0500

    nutritionix works

--- nutrition.py
+++ nutrition.py
@@ -1,22 +1,27 @@
 import urllib2
 import json
-#edamam
+from nutritionix import Nutritionix
 
-plus="%2C+"
-
-
+request = nx.search("cheese")
+result = request.json()
 #edamam has weird http errors- no authorization issues. TRY Nutritionix
+#Food4Me
+#https://github.com/leetrout/python-nutritionix
 #urllib2.HTTPError: HTTP Error 401: Unauthorize
 #http://docs.python-requests.org/en/latest/index.html
+#edamam
 
+''''
+plus="%2C+"
 base = "http://api.edamam.com/api/nutrient-info?extractOnly&url=http:http://allrecipes.com/Recipe/Albondigas&api_id=&api_key="
 print base
 request = urllib2.urlopen(base)
 result = request.read()
 print result
-
-d = json.loads(result)
+'''
+#d = json.loads(result)
+d = result
 print d
-if len(d['results']) > 0:
+if len(d) > 0:
     print "items found"
 
