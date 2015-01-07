2856ae47ae6154a0ad71831d0fae75324f86ee88
Lise <koprty@gmail.com>
2015-01-07 13:20:58 -0500

    added allergens, todo nutrition table

--- nutrition.py
+++ nutrition.py
@@ -25,6 +25,7 @@ def search(param):
         return lists
     else:
         return None
+    
 def getstats(lists):
     print "item_id "
     for item_id in lists:
@@ -32,11 +33,11 @@ def getstats(lists):
         getnutritionfacts(item_id)
         
 def getnutritionfacts(item_id):
-    l = ["allergen_contains_eggs","allergen_contains_fish","allergen_contains_gluten","allergen_contains_milk","allergen_contains_peanuts","allergen_contains_shellfish","allergen_contains_tree_nuts","allergen_contains_wheat"]
-    print l
+    allergen= ["allergen_contains_eggs","allergen_contains_fish","allergen_contains_gluten","allergen_contains_milk","allergen_contains_peanuts","allergen_contains_shellfish","allergen_contains_tree_nuts","allergen_contains_wheat", "allergen_contains_soybeans"]
+    #print allergen
     nutrifacts= nx.item(id=item_id).json()
     LT = []
-    for n in l:
+    for n in allergen:
         if nutrifacts[n] != "None":
             LT.append(n[18:])
         else:
@@ -65,7 +66,8 @@ sample output:
  u'allergen_contains_shellfish': None,
  u'allergen_contains_tree_nuts': None,
  u'allergen_contains_wheat': None,
- 
+ u'allergen_contains_soybeans': None, 
+
  u'brand_id': u'513fbc1283aa2dc80c000055',
  u'item_description': u'',
  u'item_id': u'529e7dd2f9655f6d35001d85',
@@ -73,18 +75,23 @@ sample output:
 
   u'leg_loc_id': 116,
  u'nf_calcium_dv': 10,
- u'nf_calories': 60,
+
+  u'nf_calories': 60,
  u'nf_cholesterol': 15,
  u'nf_dietary_fiber': 0,
  u'nf_iron_dv': 0,
  u'nf_monounsaturated_fat': None,
  u'nf_polyunsaturated_fat': None,
  u'nf_protein': 4,
+
  u'nf_refuse_pct': None,
+
  u'nf_saturated_fat': 3,
+
  u'nf_serving_size_qty': 1,
  u'nf_serving_size_unit': u'serving',
  u'nf_servings_per_container': None,
+
  u'nf_sodium': 90}
  u'nf_sugars': 0,
  u'nf_total_carbohydrate': 1,
@@ -92,12 +99,12 @@ sample output:
  u'nf_trans_fatty_acid': None,
  u'nf_vitamin_a_dv': 4,
  u'nf_vitamin_c_dv': 0,
+
  u'nf_water_grams': None,
  u'old_api_id': None,
  u'updated_at': u'2012-04-18T04:05:59.000Z',
  u'usda_fields': None,
-u'allergen_contains_soybeans': None, 
-u'brand_name': u'Desert Moon Grille', 
+ u'brand_name': u'Desert Moon Grille', 
 u'nf_calories_from_fat': 40,
 u'nf_serving_weight_grams': None, 
 {u'nf_ingredient_statement': None, 
