6966aebf04875acd740da02be500b6886766d2ee
Christina <christinako822@gmail.com>
2015-01-08 13:20:04 -0500

    added

--- recipes.py
+++ recipes.py
@@ -1,43 +1,56 @@
-from flask import Flask,request,url_for,redirect,render_template
 import urllib2
 import json
-app=Flask(__name__)
+from bs4 import BeautifulSoup
 url = "http://food2fork.com/api/search?key=64e7c9ab4a5b566ec0aee5ea832f1ee2&q=mozzarella%20sticks"
 request = urllib2.urlopen(url)
 result = request.read()
-#print result
 d = json.loads(result)
 #print d
 surl= ""
-@app.route("/")
-def returnRecipeURL():
-    page = ""
-    rurl= ""
-    for r in d['recipes']:
-        for i in r['source_url']:
-            rurl = rurl + i
-    return  rurl
-@app.route("/allr")
-def returnallrecipe():
-    page = ""
-    rurl= ""
-    for r in d['recipes']:
-        if r['source_url'].find("allrecipes") != -1:
-            surl= r['source_url']
-        return surl
-@app.route("/recipe")
-def returnRecipe():
-    page = ""
-    rurl= ""
-    for r in d['recipes']:
-        if r['source_url'].find("allrecipes") != -1:
-            surl= r['source_url']
-    res = urllib2.Request(surl) ##take in the source url from previous method
-    x = urllib2.urlopen(res)
-    html = x.read()
-    splitr = html.split("<ol>")
-    splitr = splitr[1].split("</ol>")
-    return "Directions:<br><ol>" + splitr[0] + "</ol>"
-if __name__=="__main__":
-    app.debug=True
-    app.run(host="0.0.0.0",port=5000)
+page = ""
+rurl= ""
+furl=""
+
+for r in d['recipes']:
+    for i in r['source_url']:
+        rurl = rurl + i
+#print rurl ## prints recipe url 
+
+for r in d['recipes']:
+    if r['source_url'].find("allrecipes") != -1:
+        surl= r['source_url']
+#print surl #prints recipe url of allrecipes (As of now)
+
+# for r in d['recipes']:
+#     if r['source_url'].find("allrecipes") != -1:
+#         surl= r['source_url']
+# res = urllib2.Request(surl) ##take in the source url from previous method
+# x = urllib2.urlopen(res)
+# html = x.read()
+# html =BeautifulSoup(html)
+#print html.find(itemprop="ingredients")
+#print html.prettify()
+#print html.get_text()
+# splitr = html.split("<ol>")
+# splitr = splitr[1].split("</ol>")
+# print "Directions:<br><ol>" + splitr[0] + "</ol>" #prints the recipe of ...
+
+##ingredients
+for r in d['recipes']:
+    if r['source_url'].find("allrecipes") != -1:
+        furl= r['f2f_url']
+res = urllib2.Request(furl) ##take in the source url from previous method
+x = urllib2.urlopen(res)
+html = x.read()
+html = BeautifulSoup(html)
+for i in html.find_all("span"):
+    if i['class'] == "plaincharacterwrap break":
+        print i.get_text()
+
+# spliti = html.split("<ul>")
+# spliti = spliti[1].split("</ul>")
+# spliti=spliti[0].split("</li>")
+# for i in spliti:
+#     print i[26:]
+    
+# #Print  "Ingredients:<br><ul>" + spliti[0] + "</ul>"   
