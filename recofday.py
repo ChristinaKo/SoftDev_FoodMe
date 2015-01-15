7cff84122e6912cd60d301ae18bd0906786932af
Christina <christinako822@gmail.com>
2015-01-15 12:55:30 -0500

    This will return a really random recipe

--- recofday.py
+++ recofday.py
@@ -6,12 +6,13 @@ url = "http://food2fork.com/api/search?key=64e7c9ab4a5b566ec0aee5ea832f1ee2&q=%s
 def rand():
     randletter = random.choice("abcdefghijklmnopqrstuvwxyz")
     randnum = random.randint(0,100)
-    url = url%(randletter, randnum)
-    request = urllib2.urlopen(url)
+    nurl = url%(randletter, randnum)
+    request = urllib2.urlopen(nurl)
     result = request.read()
     d = json.loads(result)
     if d['count'] == 0:
-        rand()
+        return rand()
     else:
-        print d['recipes'][random.randint(0,d['count'])]
+        return d['recipes'][random.randint(0,d['count'])]
+print rand()
 
