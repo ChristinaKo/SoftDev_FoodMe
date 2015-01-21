118b0c73f4cd6651940f6d5f792662e264680bf8
koprty <your_email@example.com>
2015-01-21 12:37:45 -0500

    weird Italian seasoned bread crumb

--- nutrition.py
+++ nutrition.py
@@ -4,6 +4,8 @@ from nutritionix import Nutritionix
 
 ###################KEY INFO HERE FOR API ACCESS##################################
 #you need to place an API key for Nutritionix here - provide one here below
+nx = Nutritionix (api_key = "c61a0fa95a3d990372245f601358afe3",
+                  app_id = "1634d1d7")
 ################################################################################
 
 ####### Helper Functions #######
@@ -25,22 +27,22 @@ def amountfind (item, measureu):
         except:
             print "ARRAY ERROR"
     print "from nutritionix database"
-    return float(item["fields"]["serving_size_qty"])
+    return float(item["fields"]["nf_serving_size_qty"])
         
 ##Scales nutrition facts and combines with the passed original info
 def scale (dic, factor, orig):
-    ans ={}
+    ans = {}
     x = dic.keys()
     for key in x:
         try:
             ans[key] = dic[key]*factor
-        except:
+        except: #if not num / == None, then skip
             pass
-    #None 
+     
         if len(orig) > 0: #if something in orig
             try:
                 ans[key] = orig[key] + ans[key]
-            except:
+            except: #if not orig key should i make a new one or something, or pass?? ******
                 pass            
     return ans
             
@@ -63,30 +65,47 @@ def clean (L):
 #returns one result of a search of params (using amounts and measurements as qualifiers)
 #checks measurement and amounts
 def search(param, amount, measurement):
-   # print param
+    print param
     lists=[] # list of one element id
-    request = nx.search(param,limit=100, offset=0,search_type="usda")
+    request = nx.search(param,limit=100, offset=0, search_type="usda")
     result = request.json()
     if result["total_hits"] >0:
         for item in result["hits"]: #is result hits top 10
-            if  item["fields"]["brand_name"]=="USDA" and (item["fields"]["nf_serving_size_unit"] == measurement or compare(item, measurement) ):
+            #item["fields"]["brand_name"]=="USDA" and  <- some ingredients dont have usda at least if worded differently 
+            if (item["fields"]["nf_serving_size_unit"] == measurement or compare(item, measurement)):
                 lists.append(item["fields"]["item_id"])
                 lists.append(measurement)
                 lists.append(amountfind(item,measurement))
                 return lists   # list of one element
+    item = result["hits"][1]
+    lists.append(item["fields"]["item_id"])
+    lists.append(measurement)
+    lists.append(amountfind(item,measurement))
+    return lists
+            
             
 #parses through list of item_ids and searches for nutrition facts     
 def getAstats(item_id):
     allergen= ["allergen_contains_eggs","allergen_contains_fish","allergen_contains_gluten","allergen_contains_milk","allergen_contains_peanuts","allergen_contains_shellfish","allergen_contains_tree_nuts","allergen_contains_wheat", "allergen_contains_soybeans"]
+    print item_id[0]
     nutrifacts= nx.item(id=item_id).json()
     LT = [] #List of allergens
+    print nutrifacts
     for n in allergen:
-        if nutrifacts[n] != None:
-            LT.append(n[18:])
+        try:
+            if nutrifacts[n] != None:
+                LT.append(n[18:])
+        except:
+            print n + "  ___   key DNE in this set"
+
     NF = ['nf_calories','nf_calories_from_fat','nf_total_fat','nf_saturated_fat','nf_trans_fatty_acid','nf_cholesterol','nf_sodium','nf_total_carbohydrate','nf_dietary_fiber','nf_sugars','nf_protein','nf_vitamin_a_dv','nf_vitamin_c_dv','nf_calcium_dv','nf_iron_dv']
     fact = {}
     for f in NF:
-        fact[f] = nutrifacts[f]
+        try:
+            fact[f] = nutrifacts[f]
+        except:
+            print "key error: " + f
+            
     return [fact, LT]
 
 
@@ -114,12 +133,14 @@ def parser(ingredlist):
             x.pop(0)
             searchL= clean(x)
         else:
-            measurement=0
+            measurement="serving"
             searchL=clean(x)
-            
-    #amount, measurement, searchL
+        print searchL
+        print measurement
+        print f2famount
     #search using the search params
         results = search(searchL, f2famount, measurement)
+        #print results
         resultid = results[0] #Nutritionix id of the search element
         measurement = results[1] #measurement used by the id which has been checked with measurement used in the passed recipe
         amount = results[2] #amount from Nutritionix database
@@ -139,10 +160,12 @@ def parser(ingredlist):
 ############Testing Section
 test = [' 3 skinless, boneless chicken breasts', ' 1 cup Italian seasoned bread crumbs', ' 1/2 cup grated Parmesan cheese', ' 1 teaspoon salt', ' 1 teaspoon dried thyme', ' 1 tablespoon dried basil', ' 1/2 cup butter, melted'] 
 
-#x = search("apple juice",1,"cup")
-#getNstats(x)
+#print parser(["1 cup of apple juice"])
 
-print parser(["1 cup of apple juice"])
+print getAstats(search("Italian seasoned bread crumbs", 1.0, "cup"))
+#print getAstats("54bfe04a435431322afde8c3")
+#skinless, boneless chicken breasts", 3, "serving")
+
 
 '''
 sample output:
--- recipes.py
+++ recipes.py
@@ -1,6 +1,7 @@
 import urllib2
 import json
 from bs4 import BeautifulSoup
+import nutrition
 url = "http://food2fork.com/api/search?key=64e7c9ab4a5b566ec0aee5ea832f1ee2&q=chicken%20nuggets"
 request = urllib2.urlopen(url)
 result = request.read()
@@ -23,7 +24,7 @@ html =BeautifulSoup(html)
 for i in  html.find_all("span"):
     if i.get('class') == ['plaincharacterwrap', 'break']:
         recipe.append(i.get_text())
-print recipe
+############print recipe
 ##ingredients
 res = urllib2.Request(furl) ##take in the source url from previous method
 x = urllib2.urlopen(res)
@@ -32,4 +33,5 @@ html = BeautifulSoup(html)
 for i in html.find_all("li",itemprop="ingredients"):
     ingredients.append(i.get_text())
 print ingredients
+print nutrition.parser(ingredients)
 # [u' 3 skinless, boneless chicken breasts', u' 1 cup Italian seasoned bread crumbs', u' 1/2 cup grated Parmesan cheese', u' 1 teaspoon salt', u' 1 teaspoon dried thyme', u' 1 tablespoon dried basil', u' 1/2 cup butter, melted']  --> LISE this is how it should look when it is in the array for "chicken nuggets"
