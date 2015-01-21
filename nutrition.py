e39281715c68249a405bd04968bb5ae272915846
koprty <your_email@example.com>
2015-01-21 06:49:57 -0500

    reorganized functions, NoneType error

--- nutrition.py
+++ nutrition.py
@@ -4,31 +4,15 @@ from nutritionix import Nutritionix
 
 ###################KEY INFO HERE FOR API ACCESS##################################
 #you need to place an API key for Nutritionix here - provide one here below
+nx = Nutritionix (api_key = "1f5463bda2c8668651d40bbb8b5ea1bf",
+                  app_id = "1634d1d7")
 
 ################################################################################
 
-###############################API CALL FUNCTIONS#########################################
-
-
-#'''''''''''''''''''''''''''''''''''''''''SEARCHING''''''''''''''''''''''''''''''''''''''''''''''''''''#
-#returns one result of a search of params (using amounts and measurements as qualifiers)
-#checks measurement and amounts
-def search(param, amount, measurement):
-    lists=[] # list of one element id
-    request = nx.search(param,limit=100, offset=0,search_type="usda")
-    #request = nx.search(param)
-    result = request.json()
-    if result["total_hits"] >0:
-        for item in result["hits"]: #is result hits top 10
-            if  item["fields"]["brand_name"]=="USDA" and (item["fields"]["nf_serving_size_unit"] == measurement or compare(item["fields"]["item_name"], measurement)):
-                lists.append(item["fields"]["item_id"])
-
-                return lists   # list of one element
-    return None
-
+###############################API CALL FUNCTIONS########################################
 #compares the item-name to find measurement words
-def compare(name, measureu):
-    temp = name.split()
+def compare(item, measureu):
+    temp = item["fields"]["item_name"].split()
     if measureu in temp:
         return True
     return False
@@ -42,24 +26,46 @@ def amountfind (item, measureu):
             print temp[temp.index(measureu)-1]
             return float(temp[temp.index(measureu)-1])
         except:
-            break
+            print "ARRAY ERROR"
     print "from nutritionix database"
     return float(item["fields"]["serving_size_qty"])
         
+
+#'''''''''''''''''''''''''''''''''''''''''SEARCHING''''''''''''''''''''''''''''''''''''''''''''''''''''#
+
+#returns one result of a search of params (using amounts and measurements as qualifiers)
+#checks measurement and amounts
+def search(param, amount, measurement):
+    print param
+    lists=[] # list of one element id
+    request = nx.search(param,limit=100, offset=0,search_type="usda")
+    result = request.json()
+    if result["total_hits"] >0:
+        for item in result["hits"]: #is result hits top 10
+            if  item["fields"]["brand_name"]=="USDA" and (item["fields"]["nf_serving_size_unit"] == measurement or compare(item, measurement) ):
+                lists.append(item["fields"]["item_id"])
+                lists.append(measurement)
+                lists.append(amountfind(item,measurement))
+                print "here"
+                print lists
+                return lists   # list of one element
+            
+
 #parses through list of item_ids and looks for nutrition facts     
-def getNstats(lists):
-    for item_id in lists:
-        allergen= ["allergen_contains_eggs","allergen_contains_fish","allergen_contains_gluten","allergen_contains_milk","allergen_contains_peanuts","allergen_contains_shellfish","allergen_contains_tree_nuts","allergen_contains_wheat", "allergen_contains_soybeans"]
-        nutrifacts= nx.item(id=item_id).json()
-        LT = [] #List of allergens
-        for n in allergen:
-            if nutrifacts[n] != "None":
-                LT.append(n[18:])
-            else:
-                print "error"
-        return LT
-    return "error"
-    #print nutrifacts
+def getAstats(lists):
+    allergen= ["allergen_contains_eggs","allergen_contains_fish","allergen_contains_gluten","allergen_contains_milk","allergen_contains_peanuts","allergen_contains_shellfish","allergen_contains_tree_nuts","allergen_contains_wheat", "allergen_contains_soybeans"]
+    nutrifacts= nx.item(id=item_id).json()
+    LT = [] #List of allergens
+    for n in allergen:
+        if nutrifacts[n] != "None":
+            LT.append(n[18:])
+    NF = ['nf_calories','nf_calories_from_fat','nf_total_fat','nf_saturated_fat','nf_trans_fatty_acid','nf_cholesterol','nf_sodium','nf_total_carbohydrate','nf_dietary_fiber','nf_sugars','nf_protein','nf_vitamin_a_dv','nf_vitamin_c_dv','nf_calcium_dv','nf_iron_dv']
+    fact = {}
+    for f in NF:
+        fact[f] = nutrifacts[f]
+    return [NF, LT]
+    
+      
 
 #given a brand name, will search ingredients of that brand
 def brandsearch(brand):
@@ -73,28 +79,57 @@ def brandsearch(brand):
 #''''''''''''''''''''''''''''''''''''''''''' Nutrition Calculations ''''''''''''''''''''''''''''''''''''#
 #parses through list of ingredients from food to fork and finds all nutrition facts
 def parser(ingredlist):
+    searchL = []
+    allergens = []
+    nutri= {}
+    
+    #setting up for searching
     for i in ingredlist:
-        searchL = "None"   
-        amounts = 0
-        measurement = []
         ingred = i.strip() 
     #start of parsing stuff
         x = ingred.split()
+        print i
 #ASSUMING that amount is the first element of this split list
+        f2famount=x[0]
         x.pop(0) #popping the amount 
-        amounts.append(x[0])
         if check(x[0]):
-            measurement.append(float(x[0]))
+            measurement=x[0]
+            searchL=" ".join(x[1:])
         else:
-            measurement.append(0)
-        searchL.append(parse(x)) 
+            measurement=0
+            searchL=" ".join(x)
     #amount, measurement, searchL
-        #search using the search params
-        results = search(searchL)       
-        #record correct amount
+    #search using the search params
+        results = search(searchL, f2famount, measurement)
+        print results
+        resultid = results[0]
+        print resultid
+        measurement = results[1]
+        amount = results[2]
+        scalefactor = 1.0*amount/f2famount 
+    #get nutri/allergen facts
+        stats = getAstats(resultid) #list of nutri facts, allergens
+        #combine
+        nutri = scale(stats[0], scalefactor, nutri)
+        allergen = stats[1]
+
+    print [nutri, allergen]
+    return searchL
+    #record correct amount
         
 
-
+def scale (dic, factor, orig):
+    ans ={}
+    x = dic.keys()
+    for key in x:
+        ans[key] = x[key]*factor
+        if len(orig) > 0: #if something in orig
+            try:
+                ans[key] = orig[key] + ans[key]
+            except:
+                pass            
+    return ans
+        
 #parses measurement words
 def parse(splitlist):
     if check(splitlist[0]): #check to see if word after is a measurement word, if so, remove
@@ -115,11 +150,10 @@ test = [' 3 skinless, boneless chicken breasts', ' 1 cup Italian seasoned bread 
 
 #commas dont affect number of results, but NEED TO QUALITY CHECK SEARCH RESULTS WITH COMMA
 #AMOUNTS/NUMBERS AFFECT RESULTS
-x = search("apple juice",1,"cup")
-getNstats(x)
+#x = search("apple juice",1,"cup")
+#getNstats(x)
 
-
-
+print parser(["1 cup of apple juice"])
 
 '''
 sample output:
