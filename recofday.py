17a393b9268984a71b906050773cb32717fb60da
Christina Ko <christinako822@gmail.com>
2015-01-27 15:52:01 -0500

    added api key to random

--- recofday.py
+++ recofday.py
@@ -6,9 +6,12 @@ from bs4 import BeautifulSoup
 #Angela's key: 935a5bc621fcb061d17b50ef48278d1d
 #Christina's key: 64e7c9ab4a5b566ec0aee5ea832f1ee2
 #Key 3: 73fabb20981c227717084598dff04287
+#Key #4:ea433f0fab9479fcdb2601ee80912e5c
+#key 5:c6c725b11b322d241aea51a4038c990a
 
 #url = "http://food2fork.com/api/search?key=73fabb20981c227717084598dff04287&q=%s&page=%s"
-url = "http://food2fork.com/api/search?key=64e7c9ab4a5b566ec0aee5ea832f1ee2&q=%s&page=%s"
+#url = "http://food2fork.com/api/search?key=64e7c9ab4a5b566ec0aee5ea832f1ee2&q=%s&page=%s"
+url = "http://food2fork.com/api/search?key=c6c725b11b322d241aea51a4038c990a&q=%s&page=%s"
 #url = "http://food2fork.com/api/search?key=935a5bc621fcb061d17b50ef48278d1d&q=%s&page=%s"
 def rand():
     randletter = random.choice("abcdefghijklmnopqrstuvwxyz")
